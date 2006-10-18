3469ddf72d75b87a073c279bb21bb3ad0d4066f9
gnarg <guymon@slackworks.com>
2006-10-18 04:31:22 +0000

    fixed pagination

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -8,9 +8,13 @@ class DirectoryController < ApplicationController
   def list
     shown = Person.find_all(:attribute => "show", :value => ["TRUE"], :objects => false)
     page_size = 20
-    start = (@params[:page].to_i - 1 || 0) * page_size
+    start = 0
+    if (@params[:page]) 
+      start = (@params[:page].to_i - 1) * page_size
+    end
     @pages = Paginator.new(self, shown.size, page_size, @params[:page].to_i)
     @entries = shown[start .. start + page_size].collect { |person| Person.new(person) }
   end
 
 end
+ 
