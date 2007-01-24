13198d62a7438856e272acc6f52310ca4d9fc540
gnarg <guymon@slackworks.com>
2007-01-24 22:17:55 +0000

    cut out old directory stuff

--- app/helpers/directory_helper.rb
+++
@@ -1,7 +0,0 @@
-module DirectoryHelper
-
-  def person_image(uid)
-    "<img class=\"photo\" src=\"#{url_for(:controller => 'person', :action => 'image', :id => uid)}\" alt=\"#{uid}\" />"
-  end
-  
-end
--- vendor/plugins/active_ldap/init.rb
+++ vendor/plugins/active_ldap/init.rb
@@ -1,4 +1,4 @@
-require_dependency 'active_ldap'
+require 'active_ldap'
 ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
 configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
